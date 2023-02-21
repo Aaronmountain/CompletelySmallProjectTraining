169be570239a935a1ed7a0bc4ff0db57ea31ae21
arthur <arthur.oceanus@gmail.com>
2023-02-21 23:32:23 +0800

    update gesture

--- gesture/gesture.js
+++ gesture/gesture.js
@@ -1,6 +1,7 @@
 (() => {
   /**
    * TODO: may should use pointer event is better, and add some mouse event
+   * TODO: dbClick: 單指，快速點擊兩次
    * 自定義事件，封裝手勢操作
    * P.S. 有依賴 lodash.throttle 請務必引入 lodash(沒有判斷這部分)
    * Events: 
@@ -13,20 +14,51 @@
   class Gesture {
     static STATE_STATUS = { INIT: "INIT", NONE: "NONE" }
     static EVENTS = { // 自訂定義事件 enums
-      START: "START",
-      TAP: "TAP",
-      LONG_PRESSED: "LONG_PRESSED",
-      DRAG: "DRAG",
-      PINCH: "PINCH",
-      ROTATE: "ROTATE",
-      END: "END",
+      START: 0,
+      TAP: 1,
+      LONG_PRESSED: 2,
+      DRAG: 3,
+      PINCH:4,
+      ROTATE: 5,
+      END: 6,
+    }
+    static getMatrixArr(el) {
+      const style = getComputedStyle(el, null);
+      const matrix = (
+        style.getPropertyValue("-webkit-transform") ||
+        style.getPropertyValue("-moz-transform") ||
+        style.getPropertyValue("-ms-transform") ||
+        style.getPropertyValue("-o-transform") ||
+        style.getPropertyValue("transform") ||
+        null
+      );
+
+      if (!matrix || matrix === "none") {
+        return
+      }
+
+      return matrix.split("(")[1].split(")")[0].split(",").map(Number);
+    }
+    static matrixTo(el) {
+      const mArr = this.getMatrixArr(el);
+      if (!mArr) return;
+
+      return {
+        matrix: mArr,
+        translate: [mArr[4], mArr[5]],
+        translateX: mArr[4],
+        translateY: mArr[5],
+        scale: Math.sqrt(mArr[0] ** 2, mArr[1] ** 2),
+        scaleX: mArr[0],
+        scaleY: mArr[3],
+        rotate: Math.round(Math.atan2(mArr[1], mArr[0]) * (180 / Math.PI)),
+      };
     }
 
     constructor({
       container = null,
       targetDataset = null,
-      defaultTapTime = 250,
-      defaultPressedTime = 1500,
+      defaultTapTime = 250
     }) {
       const boundaryReact = container.getBoundingClientRect();
       this.container = container; // 註冊事件的容器元素
@@ -37,8 +69,10 @@
         status: Gesture.STATE_STATUS.NONE, // 是否初始化完畢
         now: null, // 初始化當下的時間
         startMove: { x: 0, y: 0 }, // touch start 初始的 x,y 值
+        startCenter: { x: 0, y: 0 }, //
         startTouches: [], // touch start touches
         matrix: [1, 0, 0, 1, 0, 0], // 縮放矩陣
+        distance: { start: 0, end: 0 }, // 縮放比率
         dragEdge: {  // 邊界範圍
           top: boundaryReact.top,
           right: boundaryReact.right,
@@ -70,18 +104,26 @@
 
     handleStart(evt) {
       let el;
-      if (this.targetDataset && !(el = this.#getElement(evt))) {
+      if (this.targetDataset && !(el = this.getElement(evt))) {
         return;
       }
       const state = this.state;
 
       state.startTouches = evt.touches;
-      state.startMove.x = evt.touches[0].pageX;
-      state.startMove.y = evt.touches[0].pageY;
       state.now = performance.now();
-      state.matrix = this.#getTransform(el).matrix;
+      state.matrix = Gesture.getMatrixArr(el);
+
+      if (evt.touches.length === 1) {
+        state.startMove.x = evt.touches[0].pageX;
+        state.startMove.y = evt.touches[0].pageY;
+      } else if (evt.touches.length === 2) {
+        state.distance.start =
+          this.getLength(this.getVector(evt.touches[0], evt.touches[1]));
+        state.startCenter = this.getCenter(evt.touches[0], evt.touches[1]);
+      }
+
       state.status = Gesture.STATE_STATUS.INIT;
-      this.#dispatchCustomEvent({
+      this.dispatchCustomEvent({
         target: document,
         name: Gesture.EVENTS.START,
         data: {
@@ -95,7 +137,7 @@
     }
     handleMove(evt) {
       let el;
-      if (this.targetDataset && !(el = this.#getElement(evt))) {
+      if (this.targetDataset && !(el = this.getElement(evt))) {
         return;
       }
 
@@ -112,7 +154,7 @@
     }
     handleEnd(evt) {
       let el;
-      if (this.targetDataset && !(el = this.#getElement(evt))) {
+      if (this.targetDataset && !(el = this.getElement(evt))) {
         return;
       }
 
@@ -124,15 +166,14 @@
         const diffTime = performance.now() - this.state.now;
         if (diffTime < this.DefaultTapTime) {
           this.tap(el, evt);
-        } else if (diffTime < this.defaultPressedTime) {
+        } else if (diffTime < this.DefaultPressedTime) {
           this.longPressed(el, evt);
         }
         return;
       }
-
       // if (checkIsOverEdge(el, evt, state)) el.style.transform = "";
       this.state.status = Gesture.STATE_STATUS.NONE;
-      this.#dispatchCustomEvent({
+      this.dispatchCustomEvent({
         target: document,
         name: Gesture.EVENTS.END,
         data: {
@@ -144,9 +185,9 @@
       })
     }
 
-    // 點擊事件
+    // Tap
     tap(el, e) {
-      this.#dispatchCustomEvent({
+      this.dispatchCustomEvent({
         target: document,
         name: Gesture.EVENTS.TAP,
         data: {
@@ -177,13 +218,12 @@
       const currentY = e.touches[0].pageY;
       const deltaX = currentX - start.x; // x 軸位移數值
       const deltaY = currentY - start.y; // y 軸位移數值
-      const { matrix, translateX, translateY } =
-        this.#getTransform(el, ["translateX", "translateY"]);
-      matrix[4] = translateX + deltaX;
-      matrix[5] = translateY + deltaY;
+      const matrix = Gesture.getMatrixArr(el);
+      matrix[4] += deltaX;
+      matrix[5] += deltaY;
       state.matrix = matrix;
 
-      this.#dispatchCustomEvent({
+      this.dispatchCustomEvent({
         target: document,
         name: Gesture.EVENTS.DRAG,
         data: {
@@ -203,22 +243,17 @@
 
     // 縮放事件
     pinch = _.throttle((el, e, state) => {
-      const preV = {
-        x: state.startTouches[1].pageX - state.startTouches[0].pageX,
-        y: state.startTouches[1].pageY - state.startTouches[0].pageY,
-      };
-      const currentV = {
-        x: e.touches[1].pageX - e.touches[0].pageX,
-        y: e.touches[1].pageY - e.touches[0].pageY
-      };
+      state.distance.end =
+        this.getLength(this.getVector(e.touches[0], e.touches[1]));
 
-      const scale = this.#getLength(currentV) / this.#getLength(preV);
-      const matrix = this.#getTransform(el).matrix;
+      const scale =
+        parseFloat((state.distance.end / state.distance.start).toFixed(2));
+      const matrix = Gesture.matrixTo(el).matrix;
       matrix[0] = scale; // x軸縮放
       matrix[3] = scale; // y軸縮放
       state.matrix = matrix;
 
-      this.#dispatchCustomEvent({
+      this.dispatchCustomEvent({
         target: document,
         name: Gesture.EVENTS.PINCH,
         data: {
@@ -234,17 +269,21 @@
 
     // 旋轉事件
     rotate = _.throttle((el, e, state) => {
-      const v1 = this.#getVector(state.startTouches[0], state.startTouches[1]);
-      const v2 = this.#getVector(e.touches[0], e.touches[1]);
-      const rotate = parseFloat(this.#getRotateAngle(v1, v2).toFixed(2));
-      const matrix = this.#getTransform(el).matrix;
+      const v1 = this.getVector(state.startTouches[0], state.startTouches[1]);
+      const v2 = this.getVector(e.touches[0], e.touches[1]);
+      const center = this.getCenter(e.touches[0], e.touches[1]);
+
+      const rotate = parseFloat(this.getRotateAngle(v1, v2).toFixed(2));
+      const matrix = Gesture.getMatrixArr(el);
       matrix[0] *= Math.cos(rotate);
       matrix[1] = -Math.sin(rotate);
       matrix[2] = Math.sin(rotate);
       matrix[3] *= Math.cos(rotate);
+      matrix[4] = center.x;
+      matrix[5] = center.y;
       state.matrix = matrix;
 
-      this.#dispatchCustomEvent({
+      this.dispatchCustomEvent({
         target: document,
         name: Gesture.EVENTS.ROTATE,
         data: {
@@ -258,34 +297,42 @@
       state.rotate = rotate;
     }, 50);
 
-    #getVector(p1, p2) { // 取得向量座標
+    getCenter(p1, p2) { // 計算中心座標
+      return { x: (p1.pageX + p2.pageX) / 2, y: (p1.pageY + p2.pageY) / 2 };
+    }
+    getVector(p1, p2) { // 取得向量座標
       return { x: p1.pageX - p2.pageX, y: p1.pageY - p2.pageY };
     }
-    #getLength(v) { // 計算向量長度
+    getLength(v) { // 計算向量長度
       return Math.sqrt((v.x ** 2) + (v.y ** 2));
     }
-    #getDirection(v1, v2) { // 計算正轉or反轉
+    getDirection(v1, v2) { // 計算正轉or反轉
       return v1.x * v2.y - v2.x * v1.y > 0 ? -1 : 1;
     }
-    #dot(v1, v2) { // 計算向量積
+    dot(v1, v2) { // 計算向量積
       return v1.x * v2.x + v1.y * v2.y;
     }
-    #getRotateAngle(vector1, vector2) {
-      const direction = this.#getDirection(vector1, vector2);
-      const mr = this.#getLength(vector1) * this.#getLength(vector2);
+    getRotateAngle(vector1, vector2) {
+      const direction = this.getDirection(vector1, vector2);
+      const mr = this.getLength(vector1) * this.getLength(vector2);
       if (mr === 0) return 0;
 
-      let r = this.#dot(vector1, vector2) / mr;
+      let r = this.dot(vector1, vector2) / mr;
       if (r > 1) r = 1;
       if (r < -1) r = -1;
       return Math.acos(r) * direction * 180 / Math.PI;
     }
 
-    // TODO: should check drag boundaries;
-    #checkIsOverEdge(el, evt, state) {
+    checkIsOverEdge(el, evt, state) {
       // const touch = evt.touches[0];
       const targetRect = el.getBoundingClientRect();
 
+      // return (
+      //   (touch.pageX <= parentRect.left && touch.pageX + targetRect.width >= targetRect.width) ||
+      //   (touch.pageY <= parentRect.top && touch.pageY - targetRect.height <= parentRect.top) ||
+      //   (touch.pageX >= parentRect.right && touch.pageX + targetRect.width >= parentRect.right) ||
+      //   (touch.pageY >= parentRect.bottom && touch.pageY + targetRect.height >= parentRect.bottom)
+      // );
       return (
         targetRect.top < state.dragEdge.top ||
         targetRect.bottom > state.dragEdge.bottom ||
@@ -294,7 +341,7 @@
       )
     }
 
-    #getElement(evt) {
+    getElement(evt) {
       const noTarget = typeof evt.target.dataset[this.targetDataset.toLowerCase()] === 'undefined';
       return noTarget ? null : evt.target;
     }
@@ -304,68 +351,9 @@
      * name is custom event name
      * data is custom event detail
     */
-    #dispatchCustomEvent({ target, name, data }) {
+    dispatchCustomEvent({ target, name, data }) {
       target.dispatchEvent(new CustomEvent(name, { detail: data }));
     };
-
-    /**
-      keys:[
-        "translate",  "translateX",  "translateY", 
-        "scale",  "scaleX", "scalY",
-        "rotate"
-      ]
-    */
-    #getTransform(el, keys) {
-      const style = getComputedStyle(el, null);
-      const matrix = (
-        style.getPropertyValue("-webkit-transform") ||
-        style.getPropertyValue("-moz-transform") ||
-        style.getPropertyValue("-ms-transform") ||
-        style.getPropertyValue("-o-transform") ||
-        style.getPropertyValue("transform") ||
-        null
-      );
-
-      if (!matrix || matrix === "none") {
-        return
-      }
-
-      const mArr = matrix.split("(")[1].split(")")[0].split(",").map(Number);
-      const values = { matrix: mArr };
-
-      if (!keys || !keys.length) return values;
-
-      keys.forEach(key => {
-        switch (key) {
-          case "translate":
-            values["translate"] = [mArr[4], mArr[5]];
-            break;
-          case "translateX":
-            values["translateX"] = mArr[4];
-            break;
-          case "translateY":
-            values["translateY"] = mArr[5];
-            break;
-          case "scale":
-            values["scale"] =
-              Math.sqrt(mArr[0] ** 2, mArr[1] ** 2);
-            break;
-          case "scaleX":
-            values["scaleX"] = mArr[0];
-            break;
-          case "scaleY":
-            values["scaleY"] = mArr[3];
-            break;
-          case "rotate":
-            values["rotate"] =
-              Math.round(Math.atan2(mArr[1], mArr[0]) * (180 / Math.PI));
-            break;
-          default:
-            break
-        }
-      })
-      return values;
-    }
   }
 
   window.Gesture = Object.freeze(Gesture);
